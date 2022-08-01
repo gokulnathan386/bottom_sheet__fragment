23021955aaa30141b0f034efb830bec4dcd7f6ae
karuppiah <karuppiah@fusionpos.uk>
2022-08-01 12:31:34 +0530

    Initial commit

---
+++ app/src/main/java/kim/shimmer/loading/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package kim.shimmer.loading.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button OpenBottomSheet = findViewById(R.id.open_bottom_sheet);
+
+        OpenBottomSheet.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        BottomSheetDialog   gokul = new BottomSheetDialog();
+                        gokul.show(getSupportFragmentManager(),
+                                "ModalBottomSheet");
+
+                    }
+                });
+    }
+}
